be790a524e214c61d077f8e74f0b42af472ded27
Andy Armstrong <andy@hexten.net>
2014-07-24 19:17:39 +0100

    Full path to perl. May not work for you.

--- backup.sh
+++ backup.sh
@@ -2,7 +2,8 @@
 
 cd "$( dirname "$0" )"
 
-perl tools/whupup.pl sites.json
+PERL='/home/andy/perl5/perlbrew/perls/perl-5.16.0/bin/perl'
+$PERL tools/whupup.pl sites.json
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
